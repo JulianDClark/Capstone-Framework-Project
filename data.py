56f163b74121967accbdd4aff0b5819ce77fae6b
JulianDClark <125080014+JulianDClark@users.noreply.github.com>
2023-04-26 15:31:18 -0500

    created data.py file and used a dictionary array

---
+++ data.py
@@ -0,0 +1,35 @@
+quiz_data = [
+    {
+        "Question": "Cryptography is the practice and study of techniques for securing information and communication mainly to protect the data from third parties that the data is not intended for. (True/False)",
+        "correct_answer": "True",
+    },
+    {
+        "Question": "A Firewall is a network security system set on the boundaries of the system/network that monitors and controls network traffic.(True/False)",
+        "correct_answer": "True",
+    },
+    {
+        "Question": "You can only get malware from the internet. .(True/False)",
+        "correct_answer": "False",
+    },
+    {
+        "Question": "Cyberattacks are always performed by outsiders.(True/False)",
+        "correct_answer": "False",
+    },
+    {
+        "Question": "Data Leakage is an intentional or unintentional transmission of data from within the organization to an external unauthorized destination. (True/False)",
+        "correct_answer": "True",
+    },
+    {
+        "Question": "An OSI model is a reference model for how applications communicate over a network.(True/False)",
+        "correct_answer": "True",
+    },
+    {
+        "Question": "A DDOS (Distributed Denial of Service) attack is a cyberattack that causes the servers to refuse to provide services to genuine clients. (True/False)",
+        "correct_answer": "True",
+    },
+    {
+        "Question": "Security awareness training doesn’t work, it’s wasted money.(True/False)",
+        "correct_answer": "False",
+    },
+]
+        
